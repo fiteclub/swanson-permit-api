7f01186ae081313ba5d92613c0e525c22c10913d
fiteclub <mattf255@yahoo.com>
2021-05-04 22:55:37 -0400

    Generate recommendation scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_010930) do
+ActiveRecord::Schema.define(version: 2021_05_05_025326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recommendations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "rec_number"
+    t.string "issuer"
+    t.string "state"
+    t.datetime "expiration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recommendations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_05_05_010930) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "recommendations", "users"
 end
