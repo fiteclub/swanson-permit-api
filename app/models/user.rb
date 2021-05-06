e83e919bed6e954072fa471314373ae7981c09ec
fiteclub <mattf255@yahoo.com>
2021-05-05 23:43:56 -0400

    Clean up code, satisfy rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 42 }
+  validates :dob, presence: true
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :ident_state, presence: true, length: { maximum: 2 },
+                          format: { with: VALID_STATE_REGEX }
 end
