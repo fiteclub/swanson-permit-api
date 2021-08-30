fc5cf65a38ec49ac2110250560734ef401d94bda
fiteclub <mattf255@yahoo.com>
2021-08-30 13:38:01 -0400

    Upgrade gems to current

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :routing do
+RSpec.describe Api::V1::UsersController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
       expect(get: 'api/v1/users').to route_to('api/v1/users#index')
